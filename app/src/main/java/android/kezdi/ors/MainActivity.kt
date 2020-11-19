7caa3063f5db2d1626361fbc5fecde0d95169866
Hudumannaia <kezdiors@gmail.com>
2020-11-19 11:46:55 +0200

    some setup
    icon
    databinding

--- app/src/main/java/android/kezdi/ors/MainActivity.kt
+++ app/src/main/java/android/kezdi/ors/MainActivity.kt
@@ -1,11 +1,16 @@
 package android.kezdi.ors
 
+import android.kezdi.ors.databinding.ActivityMainBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
